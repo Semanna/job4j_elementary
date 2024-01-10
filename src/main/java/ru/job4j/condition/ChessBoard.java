f9e462fcff7753f406fa53b631c33bb886f9651e
Semanna <semanna.p@gmail.com>
2024-01-10 16:33:20 +0300

    4.3.6. Math.abs [#373616 #471929] - убрала переменные deltaX и deltaY

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -4,11 +4,7 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int result = 0;
         if (isValid(x1) && isValid(x2) && isValid(y1) && isValid(y2)) {
-            int deltaX = Math.abs(x2 - x1);
-            int deltaY = Math.abs(y2 - y1);
-            if (deltaX == deltaY) {
-                result = deltaX;
-            }
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) result = Math.abs(x2 - x1);
         }
         return result;
     }
