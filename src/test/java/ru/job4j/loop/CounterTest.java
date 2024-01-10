dd4ca6325f43dcbcb81037b1f3419973762a5991
Semanna <semanna.p@gmail.com>
2024-01-11 00:13:23 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192] + дополнила названия тестов для мотода sum

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 class CounterTest {
 
     @Test
-    void start15MoreThanFinish8Result0() {
+    void whenSumStart15MoreThanFinish8Result0() {
         int start = 15;
         int finish = 8;
         int expected = 0;
@@ -18,7 +18,7 @@ class CounterTest {
     }
 
     @Test
-    void start2Finish10Result54() {
+    void whenSumStart2Finish10Result54() {
         int start = 2;
         int finish = 10;
         int expected = 54;
@@ -29,12 +29,45 @@ class CounterTest {
     }
 
     @Test
-    void start1Finish1Result1() {
+    void whenSumStart1Finish1Result1() {
         int start = 1;
         int finish = 1;
         int expected = 1;
 
         int result = Counter.sum(start, finish);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersStart1Finish10Result30() {
+        int start = 1;
+        int finish = 10;
+        int expected = 30;
+
+        int result = Counter.sumByEven(start, finish);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersStart2Finish20Result110() {
+        int start = 2;
+        int finish = 20;
+        int expected = 110;
+
+        int result = Counter.sumByEven(start, finish);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersStartMinus20Finish2ResultMinus80() {
+        int start = -20;
+        int finish = 10;
+        int expected = -80;
+
+        int result = Counter.sumByEven(start, finish);
 
         assertThat(result).isEqualTo(expected);
     }
