712b7606e53252d6dd81527ef0893a1ee415d2f9
Semanna <semanna.p@gmail.com>
2024-05-19 22:03:22 +0300

    3. Массивы и модели. [#174892 #496113] - исправление замечаний

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -26,7 +26,7 @@ public class Library {
         System.out.println("==================");
         System.out.println("Show only Clean code");
         for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getQuantityOfPages() + " pages");
             }
         }
